54da18354c8508c712cab5756da32fe8cf09f1dd
903958734@qq.com <903958734@qq.com>
2020-12-22 22:06:10 +0800

    将create封装到model.ts,将clone封装到库里

--- src/models/model.ts
+++ src/models/model.ts
@@ -1,8 +1,14 @@
+import clone from "@/lib/clone";
+
 const localStorageKeyName = 'recodeList';
 const model = {
-    data:[] as RecodeItem[],
-    clone(data: RecodeItem[]|RecodeItem){
-        return JSON.parse(JSON.stringify(data));
+    data:[] as RecodeItem[],//断言类型
+    create(recode: RecodeItem) {
+        //深拷贝以后再push 到数组中，否则，
+        // recode每改变一次，loacalstorage中存着的recode都会改变，因为recode是地址，该地址里面变量值变了，所有引用这个对象的值都变了
+        const recode2: RecodeItem = clone(recode);
+        recode2.createaAt = new Date();
+        this.data.push(recode2);
     },
     fetch(){
         this.data = JSON.parse(localStorage.getItem(localStorageKeyName)||'[]') as RecodeItem[];
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -43,16 +43,12 @@ export default class Money extends  Vue{
     this.recode.notes = value;
   }
   saveRecode(){
-    //深拷贝以后再push 到数组中，否则，
-    // recode每改变一次，loacalstorage中存着的recode都会改变，因为recode是地址，该地址里面变量值变了，所有引用这个对象的值都变了
-    const recode2: RecodeItem = model.clone(this.recode);
-    recode2.createaAt = new Date();
-    this.recodeList.push(recode2);
-    //localStorage.setItem('recodeList',JSON.stringify(this.recodeList)) //不太合适，每次修改recode都会改变，应该watch
+    model.create(this.recode);
   }
+  //localStorage.setItem('recodeList',JSON.stringify(this.recodeList)) //不太合适，每次修改recode都会改变，应该watch
   @Watch('recodeList')
   onRecodeListChanged(){
-    model.save(this.recodeList)
+    model.save();
   }
 
 }
