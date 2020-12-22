5f105697d47738d20762ab39b3edf69fa3ac32dd
903958734@qq.com <903958734@qq.com>
2020-12-22 14:44:26 +0800

    添加EditLabel组件的功能，解决ID生成问题

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,15 +1,15 @@
 <template>
   <Layout>
     <div class="navBar">
-      <Icon class="leftIcon" name="left"></Icon>
+      <Icon class="leftIcon" name="left" @click.native="goBack"></Icon>
       <span class="title">编辑标签</span>
       <span class="rightIcon"></span>
     </div>
     <div class="form-wrapper">
-      <FormItem field-name="标签名" placeholder="请输入标签名"></FormItem>
+      <FormItem :value="tag.name" @update:value = "update" field-name="标签名" placeholder="请输入标签名"></FormItem>
     </div>
     <div class="button-wrapper">
-      <Button>删除标签</Button>
+      <Button @click="remove">删除标签</Button>
     </div>
 
   </Layout>
@@ -25,17 +25,36 @@ import Button from "@/components/Button.vue";
   components: {Button, FormItem}
 })
 export default class EditLabel extends Vue{
+  tag?: {id: string;name: string}= undefined;
   created(){
     const id = this.$route.params.id;
     tagListModel.fetch();
     const tags = tagListModel.data;
     const tag = tags.filter(t=>t.id===id)[0];
     if(tag){
-      console.log(tag);
+      this.tag = tag;
     }else{
       this.$router.replace('/404')
     }
   }
+  update(name:  string){
+    if (this.tag){
+      tagListModel.update(this.tag.id,name);
+    }
+  }
+  remove(){
+    if (this.tag){
+      if (tagListModel.remove(this.tag.id)) {
+        this.$router.back();
+      }else {
+        window.alert('删除失败')
+      }
+    }
+  }
+  goBack(){
+    this.$router.back();
+  }
+
 }
 </script>
 
